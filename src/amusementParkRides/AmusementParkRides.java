4aa92886835f304c127eba802398d0271397e58c
AnkitVishnoi-IT <connect2ankitvishnoi@gmail.com>
2023-11-16 19:27:44 -0500

    made chage in formatting

--- src/amusementParkRides/AmusementParkRides.java
+++ src/amusementParkRides/AmusementParkRides.java
@@ -1,17 +1,17 @@
 package amusementParkRides;
 
 public class AmusementParkRides {
-	
+
 	int height;
-	
+
 	void checkheight() {
 		if (height >= 90 && height <= 200) {
 			System.out.println("Allowed to take the ride");
-		} else {System.out.println("Not allowed to take the ride");
-		
+		} else {
+			System.out.println("Not allowed to take the ride");
+
 		}
-		
-		
-		}
+
+	}
 
 }
