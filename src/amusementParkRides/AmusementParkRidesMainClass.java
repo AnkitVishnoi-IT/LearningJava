1b38ca8c4de78a71ecf0c6cdbd24c808fbb9f6c2
AnkitVishnoi-IT <connect2ankitvishnoi@gmail.com>
2023-11-16 19:31:01 -0500

    made chage in formatting

--- src/amusementParkRides/AmusementParkRidesMainClass.java
+++ src/amusementParkRides/AmusementParkRidesMainClass.java
@@ -6,23 +6,22 @@ public class AmusementParkRidesMainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		/*AmusementParkRides allow = new AmusementParkRides();
-		allow.height = 90;
-		
-		allow.checkheight();*/
-		
-				AmusementParkRides allow = new AmusementParkRides();
-				
-				Scanner sc = new Scanner(System.in);
-				System.out.println("Enter person's height in cm : ");
-				allow.height = sc.nextInt();
-				allow.checkheight();
-				
-				sc.close();
 
-			}
+		/*
+		 * AmusementParkRides allow = new AmusementParkRides(); allow.height = 90;
+		 * 
+		 * allow.checkheight();
+		 */
+
+		AmusementParkRides allow = new AmusementParkRides();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter person's height in cm : ");
+		allow.height = sc.nextInt();
+		allow.checkheight();
+
+		sc.close();
 
 	}
 
-
+}
